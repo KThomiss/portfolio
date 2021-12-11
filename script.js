ffa1f0c0070b30ebb6b6438783cc3a7adcd3a8a9
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-12-11 15:18:04 +0100

    Layout and design choices
    Just trying out different options and learning on the way

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const changes = document.querySelectorAll(".changes");
-const reveal = document.querySelectorAll(".reveal");
+const changes = document.querySelectorAll("h5");
+const reveal = document.querySelectorAll("#mouseOverOut");
 
 for (let i = 0; i < changes.length; i++) {
-    changes[i].addEventListener("mouseover", function() {
-        reveal.forEach(function(revealItem) {
+    changes[i].addEventListener("mouseover", revealChanges);
+}
+
+function revealChanges() {
+    for (let i = 0; i < reveal.length; i++) {
+        reveal[i].style.display = "block";
+    }
+}
+
+for (let i = 0; i < changes.length; i++) {
+    changes[i].addEventListener("mouseover", function () {
+        reveal.forEach(function (revealItem) {
             revealItem.style.display = "block";
         })
     })
-    changes[i].addEventListener("mouseout", function() {
-        reveal.forEach(function(revealItem) {
+    changes[i].addEventListener("mouseout", function () {
+        reveal.forEach(function (revealItem) {
             revealItem.style.display = "none";
         })
     })
 }
+
+
+
+//event.stopPropagation();
