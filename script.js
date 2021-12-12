dbb829e9810b4e1e1787ea5451d8edb611d1761a
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-12-12 01:11:09 +0100

    Update
    Hamburger menu, some styling

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const changes = document.querySelectorAll("h5");
-const reveal = document.querySelectorAll("#mouseOverOut");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
 
-for (let i = 0; i < changes.length; i++) {
-    changes[i].addEventListener("mouseover", revealChanges);
+hamburgerBtn.onclick = function() {
+    hamburgerMenu.classList.toggle("show");
 }
-
-function revealChanges() {
-    for (let i = 0; i < reveal.length; i++) {
-        reveal[i].style.display = "block";
-    }
-}
-
-for (let i = 0; i < changes.length; i++) {
-    changes[i].addEventListener("mouseover", function () {
-        reveal.forEach(function (revealItem) {
-            revealItem.style.display = "block";
-        })
-    })
-    changes[i].addEventListener("mouseout", function () {
-        reveal.forEach(function (revealItem) {
-            revealItem.style.display = "none";
-        })
-    })
-}
-
-
-
-//event.stopPropagation();
