2e09a71b905f44ee1d7fad4f5f0930e10b8b3587
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-12-17 22:38:06 +0100

    update
    added in footer to scroll back to top and a thanks

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const hamburgerBtn = document.querySelector(".hamburger-btn");
 hamburgerBtn.onclick = function() {
     hamburgerMenu.classList.toggle("show");
 }
+
+const onMouseOver = document.querySelector(".toTop");
+const arrowToTop = document.querySelector(".arrowToTop");
+
+onMouseOver.addEventListener("mouseover", function(event) {
+    event.target.style.display = "none";
+    arrowToTop.style.display = "block";
+
+    setTimeout(function() {
+        event.target.style.display = "block";
+        arrowToTop.style.display = "none";
+    }, 2000);
+})
